4dd2959c3a055324fffe4eff6303b927f32ae01e
Spanfile <spansdev@gmail.com>
2020-10-18 18:25:33 +0300

    First bits of command parsing

--- src/management.rs
+++ src/management.rs
@@ -1,9 +1,18 @@
 use log::*;
-use serenity::{async_trait, client::Context, framework::Framework, model::channel::Message, utils::Colour};
+use serenity::{
+    async_trait, builder::CreateMessage, client::Context, framework::Framework, model::channel::Message, utils::Colour,
+};
+use structopt::{clap, StructOpt};
 
-const COMMAND_PREFIX: &str = "-ct ";
+const COMMAND_PREFIX: &str = "-ct";
 
 pub struct Management {}
+
+#[derive(StructOpt, Debug)]
+#[structopt(setting(clap::AppSettings::NoBinaryName), setting(clap::AppSettings::ColorNever), name = COMMAND_PREFIX)]
+enum Command {
+    Status,
+}
 
 #[async_trait]
 impl Framework for Management {
@@ -15,19 +24,18 @@ impl Framework for Management {
             Ok(_) => debug!("Message processed succesfully"),
             Err(err) => {
                 warn!("Message processing failed: {}", err);
-                if msg
+                if let Err(e) = msg
                     .channel_id
                     .send_message(&ctx.http, |m| {
-                        m.embed(|mut e| {
+                        m.embed(|e| {
                             e.title("An internal error has occurred")
                                 .description(err)
                                 .colour(Colour::RED)
                         })
                     })
                     .await
-                    .is_err()
                 {
-                    error!("Failed to send error message to channel {}", msg.channel_id);
+                    error!("Failed to send error message to channel {}: {}", msg.channel_id, e);
                 }
             }
         }
@@ -40,14 +48,42 @@ impl Management {
     }
 
     async fn process_message(&self, ctx: &Context, msg: &Message) -> anyhow::Result<()> {
-        if !self.should_process(msg) {
+        if !self.is_from_user(msg) {
             return Ok(());
         }
 
-        Err(anyhow::anyhow!("a deliberate error"))
+        if let Some(command) = msg.content.strip_prefix(COMMAND_PREFIX) {
+            let command = match Command::from_iter_safe(command.split_whitespace()) {
+                Ok(c) => c,
+                Err(clap::Error {
+                    kind: clap::ErrorKind::HelpDisplayed,
+                    ..
+                })
+                | Err(clap::Error {
+                    kind: clap::ErrorKind::VersionDisplayed,
+                    ..
+                }) => panic!(),
+                Err(e) => return Err(e.into()),
+            };
+
+            debug!("{:#?}", command);
+
+            let msg_builder = command.run()?;
+            msg.channel_id.send_message(&ctx.http, msg_builder).await?;
+        }
+
+        Ok(())
     }
 
-    fn should_process(&self, msg: &Message) -> bool {
-        !msg.author.bot && msg.content.starts_with(COMMAND_PREFIX)
+    fn is_from_user(&self, msg: &Message) -> bool {
+        !msg.author.bot
+    }
+}
+
+impl Command {
+    fn run<'a, 'b>(&self) -> anyhow::Result<Box<dyn FnOnce(&'b mut CreateMessage<'a>) -> &'b mut CreateMessage<'a>>> {
+        match self {
+            Command::Status => Ok(Box::new(|m| m)),
+        }
     }
 }
