192bc830b985fc2b79c06c2878dd3f0a5fa0e247
Spans <spansdev@gmail.com>
2021-08-02 20:54:55 +0300

    Tasks refactor

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ mod matcher;
 mod models;
 mod module;
 mod schema;
-// separate the embedded migrations into their own module just to the panic_in_result_fn clippy lint can be allowed in
+mod tasks;
+// separate the embedded migrations into their own module just so the panic_in_result_fn clippy lint can be allowed in
 // the entire module
 mod migrations {
     #![allow(clippy::panic_in_result_fn)]
@@ -36,13 +37,10 @@ use diesel::{
     pg::PgConnection,
     r2d2::{ConnectionManager, Pool, PooledConnection},
 };
-use error::InternalError;
-use ext::UserdataExt;
 use framework::CaretakerFramework;
 use log::*;
-use matcher::MatcherResponse;
-use module::{action::Action, cache::ModuleCache};
-use serenity::{http::Http, model::prelude::*, prelude::*, CacheAndHttp, Client};
+use module::{cache::ModuleCache};
+use serenity::{http::Http, model::prelude::*, prelude::*, Client};
 use std::{
     collections::HashMap,
     sync::Arc,
@@ -50,7 +48,6 @@ use std::{
 };
 use tokio::{
     sync::{broadcast, mpsc},
-    time,
 };
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -104,9 +101,9 @@ async fn main() -> anyhow::Result<()> {
     populate_userdata(&client, module_cache, db_pool, start_time).await?;
 
     matcher::spawn_message_matchers(msg_tx, action_tx, client.data.clone());
-    spawn_action_handler(&client, action_rx).await?;
-    spawn_shard_latency_ticker(&client, config.latency_update_freq_ms);
-    spawn_termination_waiter(&client);
+    tasks::spawn_action_handler(&client, action_rx).await?;
+    tasks::spawn_shard_latency_ticker(&client, config.latency_update_freq_ms);
+    tasks::spawn_termination_waiter(&client);
 
     info!("Starting client...");
     match client.start_autosharded().await {
@@ -166,133 +163,4 @@ async fn populate_userdata(
     data.insert::<BotUptime>(start_time);
 
     Ok(())
-}
-
-fn spawn_shard_latency_ticker(client: &Client, update_freq: u64) {
-    debug!("Spawning shard latency update ticker...");
-
-    let shard_manager = client.shard_manager.clone();
-    let client_data = client.data.clone();
-    tokio::spawn(async move {
-        debug!("Starting shard latency update loop");
-        loop {
-            time::sleep(Duration::from_millis(update_freq)).await;
-
-            let manager = shard_manager.lock().await;
-            let runners = manager.runners.lock().await;
-            let mut data = client_data.write().await;
-            let shard_meta_collection = if let Some(sm) = data.get_mut::<ShardMetadata>() {
-                sm
-            } else {
-                error!("No shard collection in client userdata");
-                continue;
-            };
-
-            for (id, runner) in runners.iter() {
-                debug!("Shard {} status: {}, latency: {:?}", id, runner.stage, runner.latency);
-
-                if let Some(meta) = shard_meta_collection.get_mut(&id.0) {
-                    match (meta.latency, runner.latency) {
-                        (_, Some(latency)) => meta.latency = Some(latency),
-                        (Some(prev), None) => {
-                            warn!("Missing latency update for shard {} (previous latency {:?})", id, prev)
-                        }
-                        (None, None) => warn!("Missing first latency update for shard {}", id),
-                    }
-                } else {
-                    error!("No metadata object for shard {} found", id);
-                }
-            }
-        }
-    });
-}
-
-fn spawn_termination_waiter(client: &Client) {
-    debug!("Spawning termination waiter...");
-
-    let shard_manager = client.shard_manager.clone();
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c().await.expect("failed to listen for SIGTERM");
-        info!("Caught SIGTERM, shutting down all shards");
-        shard_manager.lock().await.shutdown_all().await;
-    });
-}
-
-async fn spawn_action_handler(client: &Client, mut rx: mpsc::Receiver<MatcherResponse>) -> anyhow::Result<()> {
-    debug!("Spawning action handler...");
-
-    let data = client.data.read().await;
-    let module_cache = data.get_userdata::<ModuleCache>()?.clone();
-    let db_pool = data.get_userdata::<DbPool>()?.clone();
-    let cache_http = Arc::clone(&client.cache_and_http);
-
-    tokio::spawn(async move {
-        debug!("Starting action handler loop");
-        loop {
-            let (kind, msg) = if let Some(r) = rx.recv().await {
-                r
-            } else {
-                error!("Matcher response channel closed");
-                return;
-            };
-
-            let guild_id = match msg.guild_id.ok_or(InternalError::MissingGuildID) {
-                Ok(id) => id,
-                Err(e) => {
-                    error!("{}", e);
-                    continue;
-                }
-            };
-
-            let module = module_cache.get(guild_id, kind).await;
-            debug!(
-                "Running actions for guild {} module {} message {}",
-                guild_id, kind, msg.id
-            );
-
-            let db = match db_pool.get() {
-                Ok(db) => db,
-                Err(e) => {
-                    error!("Failed to get database connection from pool: {}", e);
-                    continue;
-                }
-            };
-
-            let actions = match module.get_actions(&db) {
-                Ok(a) => a,
-                Err(e) => {
-                    error!("Failed to get module actions: {}", e);
-                    continue;
-                }
-            };
-
-            for action in actions {
-                spawn_action_runner(action, Arc::clone(&cache_http), Arc::clone(&msg));
-            }
-        }
-    });
-
-    Ok(())
-}
-
-fn spawn_action_runner(action: Action<'static>, cache_http: Arc<CacheAndHttp>, msg: Arc<Message>) {
-    tokio::spawn(async move {
-        let action_dbg_display = format!("{:?}", action);
-        let start = Instant::now();
-        if let Err(e) = action.run(&cache_http, &msg).await {
-            error!(
-                "Failed to run {} against guild {:?} channel {} message {}: {}",
-                action_dbg_display, msg.guild_id, msg.channel_id, msg.id, e
-            );
-        }
-
-        debug!(
-            "Running {} against guild {:?} channel {} message {} took {:?}",
-            action_dbg_display,
-            msg.guild_id,
-            msg.channel_id,
-            msg.id,
-            start.elapsed()
-        );
-    });
 }
---
+++ src/tasks.rs
@@ -0,0 +1,134 @@
+use std::{sync::Arc, time::{Duration, Instant}};
+use log::*;
+use serenity::{CacheAndHttp, Client, model::channel::Message};
+use tokio::{sync::mpsc, time};
+use crate::{DbPool, ShardMetadata, error::InternalError, ext::UserdataExt, matcher::MatcherResponse, module::{action::Action, cache::ModuleCache}};
+
+pub fn spawn_shard_latency_ticker(client: &Client, update_freq: u64) {
+    debug!("Spawning shard latency update ticker...");
+
+    let shard_manager = client.shard_manager.clone();
+    let client_data = client.data.clone();
+    tokio::spawn(async move {
+        debug!("Starting shard latency update loop");
+        loop {
+            time::sleep(Duration::from_millis(update_freq)).await;
+
+            let manager = shard_manager.lock().await;
+            let runners = manager.runners.lock().await;
+            let mut data = client_data.write().await;
+            let shard_meta_collection = if let Some(sm) = data.get_mut::<ShardMetadata>() {
+                sm
+            } else {
+                error!("No shard collection in client userdata");
+                continue;
+            };
+
+            for (id, runner) in runners.iter() {
+                debug!("Shard {} status: {}, latency: {:?}", id, runner.stage, runner.latency);
+
+                if let Some(meta) = shard_meta_collection.get_mut(&id.0) {
+                    match (meta.latency, runner.latency) {
+                        (_, Some(latency)) => meta.latency = Some(latency),
+                        (Some(prev), None) => {
+                            warn!("Missing latency update for shard {} (previous latency {:?})", id, prev)
+                        }
+                        (None, None) => warn!("Missing first latency update for shard {}", id),
+                    }
+                } else {
+                    error!("No metadata object for shard {} found", id);
+                }
+            }
+        }
+    });
+}
+
+pub fn spawn_termination_waiter(client: &Client) {
+    debug!("Spawning termination waiter...");
+
+    let shard_manager = client.shard_manager.clone();
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("failed to listen for SIGTERM");
+        info!("Caught SIGTERM, shutting down all shards");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+}
+
+pub async fn spawn_action_handler(client: &Client, mut rx: mpsc::Receiver<MatcherResponse>) -> anyhow::Result<()> {
+    debug!("Spawning action handler...");
+
+    let data = client.data.read().await;
+    let module_cache = data.get_userdata::<ModuleCache>()?.clone();
+    let db_pool = data.get_userdata::<DbPool>()?.clone();
+    let cache_http = Arc::clone(&client.cache_and_http);
+
+    tokio::spawn(async move {
+        debug!("Starting action handler loop");
+        loop {
+            let (kind, msg) = if let Some(r) = rx.recv().await {
+                r
+            } else {
+                error!("Matcher response channel closed");
+                return;
+            };
+
+            let guild_id = match msg.guild_id.ok_or(InternalError::MissingGuildID) {
+                Ok(id) => id,
+                Err(e) => {
+                    error!("{}", e);
+                    continue;
+                }
+            };
+
+            let module = module_cache.get(guild_id, kind).await;
+            debug!(
+                "Running actions for guild {} module {} message {}",
+                guild_id, kind, msg.id
+            );
+
+            let db = match db_pool.get() {
+                Ok(db) => db,
+                Err(e) => {
+                    error!("Failed to get database connection from pool: {}", e);
+                    continue;
+                }
+            };
+
+            let actions = match module.get_actions(&db) {
+                Ok(a) => a,
+                Err(e) => {
+                    error!("Failed to get module actions: {}", e);
+                    continue;
+                }
+            };
+
+            for action in actions {
+                spawn_action_runner(action, Arc::clone(&cache_http), Arc::clone(&msg));
+            }
+        }
+    });
+
+    Ok(())
+}
+
+fn spawn_action_runner(action: Action<'static>, cache_http: Arc<CacheAndHttp>, msg: Arc<Message>) {
+    tokio::spawn(async move {
+        let action_dbg_display = format!("{:?}", action);
+        let start = Instant::now();
+        if let Err(e) = action.run(&cache_http, &msg).await {
+            error!(
+                "Failed to run {} against guild {:?} channel {} message {}: {}",
+                action_dbg_display, msg.guild_id, msg.channel_id, msg.id, e
+            );
+        }
+
+        debug!(
+            "Running {} against guild {:?} channel {} message {} took {:?}",
+            action_dbg_display,
+            msg.guild_id,
+            msg.channel_id,
+            msg.id,
+            start.elapsed()
+        );
+    });
+}
